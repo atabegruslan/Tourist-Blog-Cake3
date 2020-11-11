94c9331fc2b4b3103e0553d97566efac9c5b6bd0
atabegruslan <ruslan_aliyev_@hotmail.com>
2020-11-11 21:23:43 +0700

    admin permission see backend

--- plugins/AdminPanel/src/Controller/AppController.php
+++ plugins/AdminPanel/src/Controller/AppController.php
@@ -9,6 +9,13 @@ class AppController extends BaseController
 {
     public function beforeFilter(Event $event) 
     {
+    	if ($this->Auth->user('id') !== 1)
+    	{
+	        return $this->redirect(
+	            ['plugin' => null, 'controller' => 'Entries', 'action' => 'index']
+	        );
+    	}
+
         $this->layout = "tourist_blog_admin";
     }
 }
--- plugins/UAC/src/Controller/AppController.php
+++ plugins/UAC/src/Controller/AppController.php
@@ -6,4 +6,13 @@ use App\Controller\AppController as BaseController;
 
 class AppController extends BaseController
 {
+    public function beforeFilter(Event $event) 
+    {
+    	if ($this->Auth->user('id') !== 1)
+    	{
+	        return $this->redirect(
+	            ['plugin' => null, 'controller' => 'Entries', 'action' => 'index']
+	        );
+    	}
+    }
 }
